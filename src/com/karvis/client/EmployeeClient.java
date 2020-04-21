ddf88ff6bd15fbb7641a83a13983e756b03273b3
Karthik <karthikdy@gmail.com>
2020-04-21 13:34:01 +0530

    Added provsion for Date of birth

--- src/com/karvis/client/EmployeeClient.java
+++ src/com/karvis/client/EmployeeClient.java
@@ -5,7 +5,7 @@ import com.karvis.model.Employee;
 public class EmployeeClient {
 
 	public static void main(String[] args) {
-		Employee e = new Employee(1, "Karthik", 9004578369L, "karthikdy@gmail.com");
+		Employee e = new Employee(1, "Karthik", 9004578369L, "karthikdy@gmail.com","22/05/1992");
 		System.out.println(e.toString());
 	}
 }
--- src/com/karvis/model/Employee.java
+++ src/com/karvis/model/Employee.java
@@ -1,4 +1,9 @@
 package com.karvis.model;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * 
  * @author karthik
@@ -10,13 +15,21 @@ public class Employee {
 	String name;
 	long mobileNo;
 	String emailId;
+	Date dob;
 	
-	public Employee(long empId, String name, long mobileNo, String emailId) {
+	public Employee(long empId, String name, long mobileNo, String emailId,String dob) {
 		super();
 		this.empId = empId;
 		this.name = name;
 		this.mobileNo = mobileNo;
 		this.emailId = emailId;
+		
+		try {
+			this.dob = new SimpleDateFormat("dd/MM/yyyy").parse(dob);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			System.err.println("Date format should be dd/MM/yyyy.");
+		} 
 	}
 
 	public long getEmpId() {
@@ -50,11 +63,26 @@ public class Employee {
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
+	
+	public Date getDob() {
+		return dob;
+	}
+	
+	public String getDobAsString() {
+		return new SimpleDateFormat("dd/MM/yyyy").format(this.dob);
+	}
+	
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
 
 	@Override
 	public String toString() {
-		return "Employee [empId=" + empId + ", name=" + name + ", mobileNo=" + mobileNo + ", emailId=" + emailId + "]";
+		return "Employee [empId=" + empId + ", name=" + name + ", mobileNo=" + mobileNo + ", emailId=" + emailId
+				+ ", dob=" + this.getDobAsString() + "]";
 	}
+
+	
 	
 	
 	
